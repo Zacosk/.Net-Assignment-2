1cce08d770990d9039f8aeb3c8e45d6b8dc0168e
Zacosk <62122304+Zacosk@users.noreply.github.com>
2021-10-11 19:58:52 +1100

    Initial Commit

---
+++ dotNetAssignment2/dotNetAssignment2/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace dotNetAssignment2
+{
+    partial class LoginPage
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.userNameTxt = new System.Windows.Forms.TextBox();
+            this.passwordTxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.newUserBtn = new System.Windows.Forms.Button();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(153, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login Page";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // userNameTxt
+            // 
+            this.userNameTxt.Location = new System.Drawing.Point(182, 116);
+            this.userNameTxt.Name = "userNameTxt";
+            this.userNameTxt.Size = new System.Drawing.Size(188, 27);
+            this.userNameTxt.TabIndex = 1;
+            // 
+            // passwordTxt
+            // 
+            this.passwordTxt.Location = new System.Drawing.Point(182, 164);
+            this.passwordTxt.Name = "passwordTxt";
+            this.passwordTxt.PasswordChar = '*';
+            this.passwordTxt.Size = new System.Drawing.Size(188, 27);
+            this.passwordTxt.TabIndex = 2;
+            this.passwordTxt.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(86, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(91, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password";
+            // 
+            // newUserBtn
+            // 
+            this.newUserBtn.Location = new System.Drawing.Point(182, 238);
+            this.newUserBtn.Name = "newUserBtn";
+            this.newUserBtn.Size = new System.Drawing.Size(94, 29);
+            this.newUserBtn.TabIndex = 5;
+            this.newUserBtn.Text = "New User";
+            this.newUserBtn.UseVisualStyleBackColor = true;
+            this.newUserBtn.Click += new System.EventHandler(this.newUserBtn_Click);
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.Location = new System.Drawing.Point(49, 238);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(94, 29);
+            this.loginBtn.TabIndex = 6;
+            this.loginBtn.Text = "Login";
+            this.loginBtn.UseVisualStyleBackColor = true;
+            this.loginBtn.Click += new System.EventHandler(this.loginBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(324, 238);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(94, 29);
+            this.exitBtn.TabIndex = 7;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // LoginPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 298);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.loginBtn);
+            this.Controls.Add(this.newUserBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.passwordTxt);
+            this.Controls.Add(this.userNameTxt);
+            this.Controls.Add(this.label1);
+            this.Name = "LoginPage";
+            this.Text = "LoginPage";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox userNameTxt;
+        private System.Windows.Forms.TextBox passwordTxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button newUserBtn;
+        private System.Windows.Forms.Button loginBtn;
+        private System.Windows.Forms.Button exitBtn;
+    }
+}
+
---
+++ dotNetAssignment2/dotNetAssignment2/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace dotNetAssignment2
+{
+    public partial class LoginPage : Form
+    {
+        LinkedList<User> users = new LinkedList<User>();
+        public LoginPage()
+        {
+            InitializeComponent();
+            LoadLineDetails();
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+            string username = userNameTxt.Text;
+            string password = passwordTxt.Text;
+            if (VerifyLoginDetails(username, password))
+            {
+                MessageBox.Show("Login accepted", "Login");
+                //Form2 textForm = new Form2();
+                //textForm.Show();
+                //this.Hide();
+            }
+        }
+        private void LoadLineDetails()
+        {
+            
+            string[] fileText = File.ReadAllLines("login.txt");
+            for (int i = 0; i < fileText.Length; i++)
+            {
+                string[] splitText = fileText[i].Split(',');
+                User user = new User(splitText[0], splitText[1], splitText[2], splitText[3], splitText[4], splitText[5]);
+                users.AddLast(user);
+            }
+        }
+
+        private Boolean VerifyLoginDetails(string username, string password)
+        {
+            for (int i = 0; i < users.Count; i++)
+            {
+                User currentUser = users.ElementAt(i);
+                if (username == currentUser.username && password == currentUser.password)
+                { 
+                    //this.Hide();
+                    return true;
+                }
+            }
+            MessageBox.Show("Invalid login details", "Login", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            userNameTxt.Clear();
+            passwordTxt.Clear();
+            return false;
+        }
+
+        private void newUserBtn_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            //New form
+        }
+    }
+    class User
+    {
+        enum UserType
+        {
+            View,
+            Edit
+        }
+        public string username { get; }
+        public string password { get; }
+        string firstName, lastName, dob;
+        UserType userType;
+        public User(string username, string password, string userType, string firstName, string lastName, string dob)
+        {
+            this.username = username;
+            this.password = password;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.dob = dob;
+            this.userType = (UserType) Enum.Parse(typeof(UserType), userType);
+        }
+        string PrintableLine()
+        {
+            string text = username + "," + password + "," + userType.ToString() + "," + firstName + "," + lastName + "," + dob;
+            return text;
+        }
+    }
+}
