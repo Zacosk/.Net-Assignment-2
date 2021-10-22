6ab894a7409831621099934c300522e8c9de66fe
Zacosk <62122304+Zacosk@users.noreply.github.com>
2021-10-22 16:50:56 +1100

    Added about form and ? button to main toolbar

--- dotNetAssignment2/dotNetAssignment2/Form4.Designer.cs
+++ dotNetAssignment2/dotNetAssignment2/Form4.Designer.cs
@@ -30,6 +30,7 @@ namespace dotNetAssignment2
         private void InitializeComponent()
         {
             this.okBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // okBtn
@@ -42,20 +43,33 @@ namespace dotNetAssignment2
             this.okBtn.UseVisualStyleBackColor = true;
             this.okBtn.Click += new System.EventHandler(this.okBtn_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(355, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 140);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Rich Text Editor\r\n\r\nVersion 1.0.0.0\r\n\r\nApplication Development \r\n\r\nwith .Net Assi" +
+    "gnment 4";
+            // 
             // AboutForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(609, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.okBtn);
             this.Name = "AboutForm";
             this.Text = "About";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button okBtn;
+        private System.Windows.Forms.Label label1;
     }
 }
